e4bf81c781f1f24fe7ea74a52b92ae6543978bb6
MacWorld0096 <dudwo0096@gmail.com>
2023-02-03 14:22:51 +0900

    게시물 상세보기 구현, 날짜x

--- src/Basic_board/Main.java
+++ src/Basic_board/Main.java
@@ -72,6 +72,43 @@ public class Main {
 				System.out.printf("%d번글이 생성되었습니다.\n", id);
 				lastArticleId++;
 			}
+		
+			else if(command.startsWith("article detail ")) {
+				
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				
+//				boolean found = false;
+				Article foundArticle = null;
+				
+				for(int i = 0; i < articles.size(); i++) {
+					
+					Article article = articles.get(i);
+					if( article.id == id) {
+//						found = true;
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.printf(id + "번 글은 존재합니다.");
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2023-12-12 12:12:12\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.content);
+				
+				
+				
+				
+				
+				
+				
+			}
+			
 			else {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
